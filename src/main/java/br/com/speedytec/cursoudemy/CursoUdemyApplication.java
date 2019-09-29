2ce41386438ab2b80c8bd1df5d6f92ccfcb5b285
leandro <leandro@DESKTOP-7DBC61N>
2019-09-29 17:27:51 -0300

    Criado projeto inicial

---
+++ src/main/java/br/com/speedytec/cursoudemy/CursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.speedytec.cursoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+}
